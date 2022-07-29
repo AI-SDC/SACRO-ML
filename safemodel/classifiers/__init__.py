28df83f1c144d81228408d5a4bf8b21002ca304b
Jim Smith <james.smith@uwe.ac.uk>
2022-07-29 14:03:10 +0100

    reverted __init__.py

--- safemodel/classifiers/__init__.py
+++ safemodel/classifiers/__init__.py
@@ -1,12 +1,9 @@
 from .safedecisiontreeclassifier import SafeDecisionTreeClassifier
 from .saferandomforestclassifier import SafeRandomForestClassifier
 from .safekeras import Safe_KerasModel
-#from .safesvc import SafeSVC
-#from .dp_svc import DPSVC
+from .safesvc import SafeSVC
+from .dp_svc import DPSVC
 
-#from .safekeras import Safe_KerasModel
-#from .safetf import Safe_tf_DPModel
-
-#from .rf_binned_output_probs import RFBinnedOutput
-#from .rf_binned_output_probs import RFNoiseOutput
+from .rf_binned_output_probs import RFBinnedOutput
+from .rf_binned_output_probs import RFNoiseOutput
 
