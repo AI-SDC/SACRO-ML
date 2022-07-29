8cd38e607a5fbf43013d9aab4b9957cdb47b45d5
Jim Smith <james.smith@uwe.ac.uk>
2022-07-29 13:27:09 +0100

    commented out some of __init__.py

--- safemodel/classifiers/__init__.py
+++ safemodel/classifiers/__init__.py
@@ -1,11 +1,12 @@
 from .safedecisiontreeclassifier import SafeDecisionTreeClassifier
 from .saferandomforestclassifier import SafeRandomForestClassifier
-from .safesvc import SafeSVC
-from .dp_svc import DPSVC
-
 from .safekeras import Safe_KerasModel
-from .safetf import Safe_tf_DPModel
+#from .safesvc import SafeSVC
+#from .dp_svc import DPSVC
 
-from .rf_binned_output_probs import RFBinnedOutput
-from .rf_binned_output_probs import RFNoiseOutput
+#from .safekeras import Safe_KerasModel
+#from .safetf import Safe_tf_DPModel
+
+#from .rf_binned_output_probs import RFBinnedOutput
+#from .rf_binned_output_probs import RFNoiseOutput
 
