ede7b3fd93a0b3f4fb9acfc6a999183a6fe26210
Simon Rogers <simon.rogers@nss.nhs.scot>
2022-08-24 13:06:43 +0100

    initial work on refactoring

---
+++ attacks/dataset.py
@@ -0,0 +1,32 @@
+"""dataset.py - class to represent a dataset"""
+
+import numpy as np
+
+class Data:
+    """Stripped down Data class"""
+    def __init__(self) -> None:
+        self.name: str = ""
+        self.n_samples: int = 0
+        self.n_features: int = 0
+
+        self.x_train: np.ndarray
+        self.y_train: np.ndarray
+
+        self.x_test: np.ndarray
+        self.y_test: np.ndarray
+
+    def add_processed_data(
+        self,
+        x_train: np.ndarray,
+        y_train:np.ndarray,
+        x_test:np.ndarray,
+        y_test:np.ndarray) -> None:
+        """Add a processed and split dataset"""
+        self.x_train = x_train
+        self.y_train = y_train
+        self.x_test = x_test
+        self.y_test = y_test
+        self.n_samples = len(x_train) + len(y_train)
+
+    def __str__(self):
+        return self.name
