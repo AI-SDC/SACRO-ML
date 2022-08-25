2ec16eef248f91094b862f523e610f2be074ee0b
Andrew McCarthy <mccarthya@acm.org>
2022-08-25 07:58:59 -0700

    Merged over underscore (_) fix for SafeKerasModel

--- safemodel/classifiers/__init__.py
+++ safemodel/classifiers/__init__.py
@@ -1,11 +1,6 @@
+from .dp_svc import DPSVC
 from .safedecisiontreeclassifier import SafeDecisionTreeClassifier
+from .safekeras import SafeKerasModel
 from .saferandomforestclassifier import SafeRandomForestClassifier
 from .safesvc import SafeSVC
-from .dp_svc import DPSVC
-
-from .safekeras import Safe_KerasModel
 from .safetf import Safe_tf_DPModel
-
-from .rf_binned_output_probs import RFBinnedOutput
-from .rf_binned_output_probs import RFNoiseOutput
-
