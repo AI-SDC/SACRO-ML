64dbd5f7278188a507eb5f806076a295e009c292
Andrew McCarthy <mccarthya@acm.org>
2022-08-31 04:20:48 -0700

    Fixing pylint

--- safemodel/safemodel.py
+++ safemodel/safemodel.py
@@ -49,7 +49,7 @@ def check_min(key: str, val: Any, cur_val: Any) -> tuple[str, bool]:
 
 
     """
-    if (isinstance(cur_val,int) or isinstance(cur_val,float)):
+    if (isinstance(cur_val,(int,float)):
         if cur_val < val:
             disclosive = True
             print(f'key = {key}')
@@ -64,7 +64,10 @@ def check_min(key: str, val: Any, cur_val: Any) -> tuple[str, bool]:
             disclosive = False
             msg = ""
         return msg, disclosive
-
+    else:
+        disclosive = True
+        msg = get_reporting_string()
+        return msg, disclosive
 
 def check_max(key: str, val: Any, cur_val: Any) -> tuple[str, bool]:
     """Checks maximum value constraint.
@@ -93,7 +96,7 @@ def check_max(key: str, val: Any, cur_val: Any) -> tuple[str, bool]:
 
 
     """
-    if (isinstance(cur_val,int) or isinstance(cur_val,float)):
+    if (isinstance(cur_val,(int,float)):
         if cur_val > val:
             disclosive = True
             msg = get_reporting_string(name="greater_than_max_value", key=key,
@@ -107,8 +110,8 @@ def check_max(key: str, val: Any, cur_val: Any) -> tuple[str, bool]:
             msg = ""
         return msg, disclosive
     else:
-        disclosive = False
-        msg = ""
+        disclosive = True
+        msg = get_reporting_string()
         return msg, disclosive
 
 
