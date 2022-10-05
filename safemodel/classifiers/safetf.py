5d03ffcf8bff33800dd333548c2430e1d57684b6
Jim-smith <jim-smith@users.noreply.github.com>
2022-10-05 17:14:20 +0100

    linting

--- safemodel/classifiers/safetf.py
+++ safemodel/classifiers/safetf.py
@@ -1,20 +1,31 @@
+""" work in progress to allow use of the DPModel classes
+ Jim smith 2022
+ When ready, linting of  the imports will be enabled
+"""
+
+
+#pylint: disable=unused-import
+from typing import Any
 import tensorflow as tf
 import tensorflow_privacy as tf_privacy
 
-from tensorflow.keras import Model as KerasModel
-from safemodel.safemodel import SafeModel
+#from tensorflow.keras import Model as KerasModel
 from tensorflow_privacy.privacy.analysis import compute_dp_sgd_privacy
 from tensorflow_privacy.privacy.optimizers import dp_optimizer_keras
 
-import tensorflow_privacy as tf_privacy
 from tensorflow_privacy import DPModel
-from typing import Any
 
+from safemodel.safemodel import SafeModel
 
 class Safe_tf_DPModel(SafeModel, DPModel):
     """ Privacy Protected tensorflow_privacy DP-SGD subclass of Keras model"""
-
-    def __init__(self,l2_norm_clip:float, noise_multiplier:float, use_xla:bool=True, *args:any, **kwargs:any) ->None:
+    #remove comment once model starts to be populated
+    #pylint:disable=super-init-not-called
+    def __init__(self,l2_norm_clip:float,
+                 noise_multiplier:float,
+                 use_xla:bool,
+                 *args:any,
+                 **kwargs:any) ->None:
         """creates model and applies constraints to parameters"""
         #safemodel.__init__(self)
         #DPModel.__init__(self, **kwargs)
