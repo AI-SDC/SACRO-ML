eed93880a9d083319d5b60116fc68acfca294c9f
Jim-smith <jim-smith@users.noreply.github.com>
2022-11-01 00:42:10 +0000

    more test coverage in attacks module

--- tests/test_attacks.py
+++ tests/test_attacks.py
@@ -1,7 +1,10 @@
 """Jim Smith October 2022"""
+import math
+
+import numpy as np
 import pytest
 
-from attacks import attack, dataset
+from attacks import attack, dataset, mia_extremecase
 from safemodel.classifiers import SafeDecisionTreeClassifier
 
 
@@ -14,3 +17,23 @@ def test_superclass():
         my_attack.attack(target_obj, dataset_obj)
     with pytest.raises(NotImplementedError):
         print(str(my_attack))  # .__str__()
+
+
+def test_mia_extremecase():
+    """test the extreme case mia in the file of the same name"""
+
+    # create actual values
+    y = np.zeros(50000)
+    y[:25] = 1
+    # exactly right and wrong predictions
+    right = np.zeros(50000)
+    right[:25] = 1
+    wrong = 1 - right
+
+    # right predictions - triggers override for very small logp
+    _, _, _, pval = mia_extremecase.min_max_disc(y, right)
+    assert pval == -115.13
+
+    # wrong predictions - probaility very close to 1 so logp=0
+    _, _, _, pval = mia_extremecase.min_max_disc(y, wrong)
+    assert math.isclose(pval, 0.0)
