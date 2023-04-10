ebb961138958a395ab24e7ff788df6aa9cda9901
Shahzad Mumtaz <53081016+shahzadmumtaz22@users.noreply.github.com>
2023-04-10 11:31:24 +0100

    Adding uuid package

--- aisdc/attacks/worst_case_attack.py
+++ aisdc/attacks/worst_case_attack.py
@@ -13,6 +13,7 @@ from typing import Any
 
 import numpy as np
 import sklearn
+import uuid
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
