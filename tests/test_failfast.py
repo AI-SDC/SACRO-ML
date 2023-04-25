eb26a34372b78b552c02d20bee7d3f7d96db4c22
Shahzad Mumtaz <53081016+shahzadmumtaz22@users.noreply.github.com>
2023-04-25 21:09:48 +0100

    Update test_failfast.py

--- tests/test_failfast.py
+++ tests/test_failfast.py
@@ -2,7 +2,6 @@
 Copyright (C) Jim Smith 2022 <james.smith@uwe.ac.uk>
 """
 from unittest.mock import patch
-
 import pytest
 
 from aisdc.attacks import (  # pylint: disable = import-error
@@ -10,21 +9,11 @@ from aisdc.attacks import (  # pylint: disable = import-error
     worst_case_attack,
 )
 
-
 def test_check_attack_success():
     """removes unwanted files or directory"""
     metrics={}
     metrics["AUC"]=0.80
     metrics["ACC"]=0.90
-
-    # Option 1: AUC with greater than or equal to
-    args = worst_case_attack.WorstCaseAttackArgs(
-        attack_metric_success_name="AUC",
-        attack_metric_success_thresh=0.6,
-        attack_metric_success_comp_type="gte",
-    )
-    failfast_metric_summary = FailFast(args)
-    assert failfast_metric_summary.check_attack_success(metrics) is True
 
     # Option 1: AUC with greater than or equal to
     args = worst_case_attack.WorstCaseAttackArgs(
