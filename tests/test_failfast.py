5d86ae7460c87885fab731d9e7aa055f80038256
Shahzad Mumtaz <53081016+shahzadmumtaz22@users.noreply.github.com>
2023-04-25 22:42:50 +0100

    Update test_failfast.py

--- tests/test_failfast.py
+++ tests/test_failfast.py
@@ -27,3 +27,95 @@ def test_parse_boolean_argument():
     )
     failfast_Obj = failfast.FailFast(args)
     assert failfast_Obj.check_attack_success(metrics) is True
+
+    # Option 3
+    args = worst_case_attack.WorstCaseAttackArgs(
+        attack_metric_success_name="AUC",
+        attack_metric_success_thresh=0.6,
+        attack_metric_success_comp_type="lte",
+    )
+    failfast_Obj = failfast.FailFast(args)
+    assert failfast_Obj.check_attack_success(metrics) is False
+
+    # Option 4
+    args = worst_case_attack.WorstCaseAttackArgs(
+        attack_metric_success_name="AUC",
+        attack_metric_success_thresh=0.99,
+        attack_metric_success_comp_type="lte",
+    )
+    failfast_Obj = failfast.FailFast(args)
+    assert failfast_Obj.check_attack_success(metrics) is True
+
+    # Option 5
+    args = worst_case_attack.WorstCaseAttackArgs(
+        attack_metric_success_name="AUC",
+        attack_metric_success_thresh=0.6,
+        attack_metric_success_comp_type="gte",
+    )
+    failfast_Obj = failfast.FailFast(args)
+    assert failfast_Obj.check_attack_success(metrics) is True
+
+    # Option 6
+    args = worst_case_attack.WorstCaseAttackArgs(
+        attack_metric_success_name="AUC",
+        attack_metric_success_thresh=0.99,
+        attack_metric_success_comp_type="gte",
+    )
+    failfast_Obj = failfast.FailFast(args)
+    assert failfast_Obj.check_attack_success(metrics) is False
+
+    # Option 7
+    args = worst_case_attack.WorstCaseAttackArgs(
+        attack_metric_success_name="AUC",
+        attack_metric_success_thresh=0.6,
+        attack_metric_success_comp_type="gt",
+    )
+    failfast_Obj = failfast.FailFast(args)
+    assert failfast_Obj.check_attack_success(metrics) is True
+
+    # Option 8
+    args = worst_case_attack.WorstCaseAttackArgs(
+        attack_metric_success_name="AUC",
+        attack_metric_success_thresh=0.99,
+        attack_metric_success_comp_type="gt",
+    )
+    failfast_Obj = failfast.FailFast(args)
+    assert failfast_Obj.check_attack_success(metrics) is False
+
+    # Option 9
+    args = worst_case_attack.WorstCaseAttackArgs(
+        attack_metric_success_name="AUC",
+        attack_metric_success_thresh=0.8,
+        attack_metric_success_comp_type="eq",
+    )
+    failfast_Obj = failfast.FailFast(args)
+    assert failfast_Obj.check_attack_success(metrics) is True
+
+    # Option 10
+    args = worst_case_attack.WorstCaseAttackArgs(
+        attack_metric_success_name="AUC",
+        attack_metric_success_thresh=0.6,
+        attack_metric_success_comp_type="eq",
+    )
+    failfast_Obj = failfast.FailFast(args)
+    assert failfast_Obj.check_attack_success(metrics) is False
+
+    # Option 11
+    args = worst_case_attack.WorstCaseAttackArgs(
+        attack_metric_success_name="AUC",
+        attack_metric_success_thresh=0.8,
+        attack_metric_success_comp_type="not_eq",
+    )
+    failfast_Obj = failfast.FailFast(args)
+    assert failfast_Obj.check_attack_success(metrics) is False
+
+    # Option 12
+    args = worst_case_attack.WorstCaseAttackArgs(
+        attack_metric_success_name="AUC",
+        attack_metric_success_thresh=0.6,
+        attack_metric_success_comp_type="not_eq",
+    )
+    failfast_Obj = failfast.FailFast(args)
+    assert failfast_Obj.check_attack_success(metrics) is True
+
+    assert failfast_Obj.get_fail_count() == 0
