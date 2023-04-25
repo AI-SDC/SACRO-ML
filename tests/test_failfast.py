453612a897747cf7be2b94ba588091f6dd01f84e
Shahzad Mumtaz <53081016+shahzadmumtaz22@users.noreply.github.com>
2023-04-25 21:06:39 +0100

    Update test_failfast.py
    Signed-off-by: Shahzad Mumtaz <53081016+shahzadmumtaz22@users.noreply.github.com>

--- tests/test_failfast.py
+++ tests/test_failfast.py
@@ -2,42 +2,16 @@
 Copyright (C) Jim Smith 2022 <james.smith@uwe.ac.uk>
 """
 from unittest.mock import patch
-
 import pytest
-
-<<<<<<< HEAD
-from aisdc.attacks import (  # pylint: disable = import-error
-    failfast.FailFast,
-    worst_case_attack,
-)
-
+from aisdc.attacks import worst_case_attack, failfast.FailFast  # pylint: disable = import-error
 
 def test_check_attack_success():
     """removes unwanted files or directory"""
     metrics={}
     metrics["AUC"]=0.80
     metrics["ACC"]=0.90
-
-    # Option 1: AUC with greater than or equal to
-=======
-from sklearn.datasets import load_breast_cancer
-from sklearn.model_selection import train_test_split
-from sklearn.svm import SVC
-
-from aisdc.attacks import (  # pylint: disable = import-error
-    dataset,
-    failfast,
-    worst_case_attack,
-)
-
-
-def test_check_attack_success():
-    """removes unwanted files or directory"""
-    metrics["AUC"] = 0.80
-    metrics["ACC"] = 0.90
-
-    # Option 1: AUC with greater than or equal to
->>>>>>> c9f5d7340616a28375d5fbd5db3b3f34a6a2aedd
+    
+    # Option 1: AUC with greater than or equal to 
     args = worst_case_attack.WorstCaseAttackArgs(
         attack_metric_success_name="AUC",
         attack_metric_success_thresh=0.6,
