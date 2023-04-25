f31ffd50624f64fd34f39fff38ab407d1b45d51e
Shahzad Mumtaz <53081016+shahzadmumtaz22@users.noreply.github.com>
2023-04-25 20:53:12 +0100

    Create test_failfast.py

---
+++ tests/test_failfast.py
@@ -0,0 +1,64 @@
+"""test_worst_case_attack.py
+Copyright (C) Jim Smith 2022 <james.smith@uwe.ac.uk>
+"""
+import os
+import sys
+from unittest.mock import patch
+
+import numpy as np
+import pytest
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+
+from aisdc.attacks import dataset, worst_case_attack, failfast  # pylint: disable = import-error
+
+def test_check_attack_success():
+    """removes unwanted files or directory"""
+    metrics["AUC"]=0.80
+    metrics["ACC"]=0.90
+    
+    # Option 1: AUC with greater than or equal to 
+    args = worst_case_attack.WorstCaseAttackArgs(
+    attack_metric_success_name="AUC",
+    attack_metric_success_thresh=0.6,
+    attack_metric_success_comp_type="gte",    
+    )
+    failfast_metric_summary = FailFast(args)    
+    assert failfast_metric_summary.check_attack_success(metrics) is True
+
+    # Option 1: AUC with greater than or equal to 
+    args = worst_case_attack.WorstCaseAttackArgs(
+    attack_metric_success_name="AUC",
+    attack_metric_success_thresh=0.99,
+    attack_metric_success_comp_type="gte",    
+    )
+    failfast_metric_summary = FailFast(args)    
+    assert failfast_metric_summary.check_attack_success(metrics) is False
+
+    # Option 2: AUC with greater than or equal to 
+    args = worst_case_attack.WorstCaseAttackArgs(
+    attack_metric_success_name="AUC",
+    attack_metric_success_thresh=0.6,
+    attack_metric_success_comp_type="gte",    
+    )
+    failfast_metric_summary = FailFast(args)    
+    assert failfast_metric_summary.check_attack_success(metrics) is True
+
+    # Option 3: AUC with greater than or equal to 
+    args = worst_case_attack.WorstCaseAttackArgs(
+    attack_metric_success_name="AUC",
+    attack_metric_success_thresh=0.6,
+    attack_metric_success_comp_type="lte",    
+    )
+    failfast_metric_summary = FailFast(args)    
+    assert failfast_metric_summary.check_attack_success(metrics) is False
+
+    # Option 4: AUC with greater than or equal to 
+    args = worst_case_attack.WorstCaseAttackArgs(
+    attack_metric_success_name="AUC",
+    attack_metric_success_thresh=0.99,
+    attack_metric_success_comp_type="lte",    
+    )
+    failfast_metric_summary = FailFast(args)    
+    assert failfast_metric_summary.check_attack_success(metrics) is True
