1fa7560e7290749741cd5656b97e8079add9a04b
yolaj-nhs <yola.jones@nhs.scot>
2023-04-27 13:31:49 +0100

    Fixing linting

--- aisdc/attacks/likelihood_attack.py
+++ aisdc/attacks/likelihood_attack.py
@@ -169,7 +169,7 @@ class LIRAAttack(Attack):
 
         return dataset
 
-    def run_scenario_from_preds(  # pylint: disable = too-many-statements
+    def run_scenario_from_preds(  # pylint: disable = too-many-statements, too-many-arguments, too-many-locals
         self,
         shadow_clf: sklearn.base.BaseEstimator,
         X_target_train: Iterable[float],
