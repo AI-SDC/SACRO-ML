fe0c2fe92d43200c399bd7597849f98f2cb4d2a2
Shahzad Mumtaz <53081016+shahzadmumtaz22@users.noreply.github.com>
2023-05-03 08:00:20 +0100

    Update worst_case_attack.py

--- aisdc/attacks/worst_case_attack.py
+++ aisdc/attacks/worst_case_attack.py
@@ -851,7 +851,7 @@ def main():
         required=False,
         dest="attack_fail_fast",
         help=(
-            """to stop further repetitions when the given metric has fullfilled
+            """to stop further repetitions when the given metric has fulfilled
             a criteria for a specified number of times (--attack-metric-success-count-thresh) 
             and this has a true status.
             Default = %(default)s"""
