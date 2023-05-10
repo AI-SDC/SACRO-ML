b277eb2136f9b0def6f4f494752013caef770c2c
Shahzad Mumtaz <53081016+shahzadmumtaz22@users.noreply.github.com>
2023-05-10 11:50:24 +0100

    Update test_lira_attack.py

--- tests/test_lira_attack.py
+++ tests/test_lira_attack.py
@@ -54,6 +54,42 @@ def test_lira_attack():
     _ = output2["attack_metrics"][0]
 
 
+def test_lira_attack_failfast():
+    """tests the lira code two ways"""
+    args = LIRAAttackArgs(
+        n_shadow_models=100, 
+        report_name="lira_example_report" 
+        shadow_models_fail_fast=True,
+        n_shadow_rows_confidences_min=10,
+        )
+    attack_obj = LIRAAttack(args)
+    attack_obj.setup_example_data()
+    attack_obj.attack_from_config()
+    attack_obj.example()
+
+    X, y = load_breast_cancer(return_X_y=True, as_frame=False)
+    train_X, test_X, train_y, test_y = train_test_split(X, y, test_size=0.3)
+    dataset = Data()
+    dataset.add_processed_data(train_X, train_y, test_X, test_y)
+
+    # target_model = SVC(gamma=0.1, probability=True)
+    target_model = RandomForestClassifier(
+        n_estimators=100, min_samples_split=2, min_samples_leaf=1
+    )
+    target_model.fit(train_X, train_y)
+
+    args2 = LIRAAttackArgs(
+        n_shadow_models=100, 
+        report_name="lira_example3_failfast_report"
+        shadow_models_fail_fast=True,
+        n_shadow_rows_confidences_min=10,
+        )
+    attack_obj2 = LIRAAttack(args2)
+    attack_obj2.attack(dataset, target_model)
+    output2 = attack_obj2.make_report()  # also makes .pdf and .json files
+    _ = output2["attack_metrics"][0]
+
+
 def test_check_and_update_dataset():
     """test the code that removes items from test set with classes
     not present in training set"""
