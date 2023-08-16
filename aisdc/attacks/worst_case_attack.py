aa0bcc8e8f9b18404e2db73a46c76ad8bc9467f2
Alba <crespialba@gmail.com>
2023-08-16 11:17:39 +0100

    pylint fix
    fixed pylint issues

--- aisdc/attacks/worst_case_attack.py
+++ aisdc/attacks/worst_case_attack.py
@@ -2,7 +2,7 @@
 Worst_case_attack.py.
 
 Runs a worst case attack based upon predictive probabilities stored in two .csv files
-"""
+""" # pylint: disable = too-many-lines
 
 from __future__ import annotations
 
@@ -11,7 +11,7 @@ import logging
 import os
 import uuid
 from datetime import datetime
-from typing import Any
+from typing import Any, Iterable, Union
 
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
@@ -38,7 +38,7 @@ class WorstCaseAttack(Attack):
     def __init__(  # pylint: disable = too-many-arguments, too-many-locals
         self,
         n_reps: int = 10,
-        reproduce_split: Union[int, Iterable[int], None] = 5, #if list parsed, needs to be of length n_reps
+        reproduce_split: Union[int, Iterable[int], None] = 5,
         p_thresh: float = 0.05,
         n_dummy_reps: int = 1,
         train_beta: int = 1,
@@ -132,19 +132,22 @@ class WorstCaseAttack(Attack):
         self.n_reps = n_reps
         self.reproduce_split = reproduce_split
         if isinstance(reproduce_split, int):
-            reproduce_split = [reproduce_split]
-            reproduce_split += [x**2 for x in range(reproduce_split[0], reproduce_split[0]+n_reps-1)] #re-assign the value of reproduce_split to be ready for the next round
+            reproduce_split = [reproduce_split] + [x**2 for x in range(reproduce_split, reproduce_split+n_reps-1)]# pylint: disable = line-too-long
         else:
             reproduce_split = list(dict.fromkeys(reproduce_split)) #remove potential duplicates
             if len(reproduce_split)==n_reps:
-               pass
+                pass
             elif len(reproduce_split)>n_reps:
                 print('split',reproduce_split, 'nreps',n_reps)
                 reproduce_split = list(reproduce_split)[0:n_reps]
-                print("WARNING: the length of the parameter 'reproduce_split' is longer than n_reps. Values have been removed.")
-            else: 
-                reproduce_split += [reproduce_split[-1]*x for x in range(2, (n_reps-len(reproduce_split)+2))] #assign values to match length of n_reps
-                print("WARNING: the length of the parameter 'reproduce_split' is shorter than n_reps. Vales have been added.")
+                print("WARNING: the length of the parameter 'reproduce_split'\
+                     is longer than n_reps. Values have been removed.")
+            else:
+                #assign values to match length of n_reps
+                reproduce_split += [reproduce_split[-1]*x
+                                   for x in range(2, (n_reps-len(reproduce_split)+2))]
+                print("WARNING: the length of the parameter 'reproduce_split'\
+                     is shorter than n_reps. Vales have been added.")
             print('reproduce split now', reproduce_split)
         self.reproduce_split = reproduce_split
         self.p_thresh = p_thresh
@@ -350,12 +353,12 @@ class WorstCaseAttack(Attack):
         mia_metrics = []
 
         failfast_metric_summary = FailFast(self)
-        
+
         for rep in range(self.n_reps):
             logger.info("Rep %d of %d split %d", rep + 1, self.n_reps, self.reproduce_split[rep])
             mi_train_x, mi_test_x, mi_train_y, mi_test_y = train_test_split(
                 mi_x, mi_y, test_size=self.test_prop, stratify=mi_y,
-                random_state=self.reproduce_split[rep],  shuffle=True
+                random_state=self.reproduce_split[rep], shuffle=True
             )
             attack_classifier = self.mia_attack_model(**self.mia_attack_model_hyp)
             attack_classifier.fit(mi_train_x, mi_train_y)
