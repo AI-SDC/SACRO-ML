798a358b50671c7ae2200c28d7a266f99efc0425
Simon Rogers <simon.rogers@nss.nhs.scot>
2023-11-07 11:45:35 +0000

    added comments

--- violin-plot.py
+++ violin-plot.py
@@ -1,4 +1,9 @@
-# %%
+'''WIP code to create plots for the SACRO experiments
+Creates a seaborn facet grid where rows are attack metrics
+and columns are structural metrics. Within each plot in the grid there is one
+paired violin plot per dataset and each paired violin plot separates the data points
+based on the structural risk value'''
+# %% Import libraries
 import os
 import glob
 import logging
@@ -6,19 +11,27 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 
+# %% Setup logging and seaborn
 sns.set_theme(style="dark")
+sns.set(font_scale=2.0)
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-DATA_PATH = '/Users/simonr04/sacro_results'
-GLOB_PATTERN = 'DT_*.csv'
-P_THRESH = 0.05
-# %%
+
+# %% Parameters to modify
+DATA_PATH = '/Users/simonr04/sacro_results' # where are the results .csv files
+GLOB_PATTERN = 'DT_*.csv' # which results .csv files to load
+# GLOB_PATTERN = 'xgboost_*.csv'
+P_THRESH = 0.05 # Threshold for binary p-value significance evaluation
+ATTACK_SCENARIO = 'lira' # Which Attack scenario are we analysing
+
+# %% Retrieve the files to load
 file_list = glob.glob(
     os.path.join(DATA_PATH, GLOB_PATTERN)
 )
 logger.info("Found %d files", len(file_list))
-# %%
+
+# %% Load the files
 total_df = None
 for filename in file_list:
     logger.info("Reading %s", filename)
@@ -31,14 +44,14 @@ for filename in file_list:
     logger.info("Total rows = %d", len(total_df))
 
 # %% Summarise over repetitions
+# Within the .csv files, each experiments is repeated, with an index in the repetitions column
+# Some columns are constant over repetitions -- when we summarise these we just keep the first value
+# Some columns change and need to be combined. A **mean** is used. TODO: is this best?
+
+# These are the columns that will be combined via **mean**
 COLS_TO_AVERAGE = [
-    'attack_TPR',
-    'attack_FPR',
-    'attack_FAR',
-    'attack_TNR',
-    'attack_PPV',
-    'attack_NPV',
-    'attack_FNR',
+    'attack_TPR', 'attack_FPR', 'attack_FAR', 'attack_TNR',
+    'attack_PPV', 'attack_NPV', 'attack_FNR',
     'attack_ACC', 'attack_F1score', 'attack_Advantage',
     'attack_AUC', 'attack_P_HIGHER_AUC', 'attack_FMAX01', 'attack_FMIN01',
     'attack_FDIF01', 'attack_PDIF01', 'attack_FMAX02', 'attack_FMIN02',
@@ -51,28 +64,42 @@ COLS_TO_AVERAGE = [
     'attack_k_anonymity_risk', 'attack_class_disclosure_risk',
     'attack_unnecessary_risk', 'attack_lowvals_cd_risk'
 ]
-CONSTANT_COLS = ['dataset',
-                 # 'scenario',
-                  'target_classifier', 'target_clf_file',
-       'attack_classifier', 'attack_clf_file', 'repetition',
-       'target_generalisation_error', 
-       #'model_data_param_id', 
-       'param_id',
-       'criterion', 'splitter', 'max_depth', 'min_samples_split',
-       'min_samples_leaf', 'max_features', 'class_weight', 'attack_TPR',
-    'attack_n_pos_test_examples',
-       'attack_n_neg_test_examples', 'mia_hyp_min_samples_split',
-       'mia_hyp_min_samples_leaf', 'mia_hyp_max_depth']
 
+# These are the columns that are constant (or don't matter), we just take the first value
+# If only files from a single classifier are used, then these will be classifier specific
+# TODO: make classifier-specific lists and then select accordingly?
+CONSTANT_COLS = [ # decision tree columns
+        'dataset',
+        'target_classifier', 'target_clf_file',
+        'attack_classifier', 'attack_clf_file', 'repetition',
+        'target_generalisation_error', 
+        #'model_data_param_id', 
+        'param_id',
+        'criterion', 'splitter', 'max_depth', 'min_samples_split',
+        'min_samples_leaf', 'max_features', 'class_weight', 'attack_TPR',
+        'attack_n_pos_test_examples',
+        'attack_n_neg_test_examples', 'mia_hyp_min_samples_split',
+        'mia_hyp_min_samples_leaf', 'mia_hyp_max_depth'
+]
+
+# And this for xgboost
+# CONSTANT_COLS = ['dataset', 'target_classifier', 'target_clf_file',
+#        'attack_classifier', 'attack_clf_file', 'repetition',
+#        'target_generalisation_error', 'param_id',
+#        'n_estimators', 'max_depth', 'booster', 'use_label_encoder',
+#        'min_child_weight', 'verbosity']
+
+# Make an aggregation object and perform the aggregations
 aggregations = {}
 for col_name in CONSTANT_COLS:
     aggregations[col_name] = 'first' # any agg will work
 for col_name in COLS_TO_AVERAGE:
     aggregations[col_name] = 'mean'
 
-agg_df = total_df.groupby(['scenario', 'model_data_param_id']).agg(aggregations).reset_index(inplace=False)
+agg_df = total_df.groupby(['scenario', 'model_data_param_id']).agg(aggregations)
+agg_df.reset_index(inplace=True)
 
-# Check the sizes
+# Check the sizes of the result make sure all aggregation has worked correctly - good for spotting bugs
 n_reps_worstcase = len(total_df[total_df['scenario'] == 'WorstCase']['repetition'].unique())
 n_reps_lira = len(total_df[total_df['scenario'] == 'lira']['repetition'].unique())
 n_reps_structural = len(total_df[total_df['scenario'] == 'Structural']['repetition'].unique())
@@ -91,10 +118,13 @@ n_rows_actual = len(agg_df)
 assert n_rows_actual == n_rows_predicted
 
 # %% Add any additional columns required
+# Here two additional metrics are added with binary significance values in
 agg_df['attack_sig_AUC'] = (agg_df['attack_P_HIGHER_AUC'] <= P_THRESH)
 agg_df['attack_sig_FDIF01'] = (np.exp(-agg_df['attack_PDIF01']) <= P_THRESH)
 
-# %% Define some useful things
+# %% Define the metrics of interest
+
+# These are the structural metrics to use (all of them)
 structural_metrics = [
     'attack_k_anonymity_risk',
     'attack_DoF_risk',
@@ -103,39 +133,65 @@ structural_metrics = [
     'attack_lowvals_cd_risk'
 ]
 
+# These are the attack metrics to use (add others / remove / whatever)
 attack_metrics = [
     'attack_AUC',
     'attack_FDIF01',
     'attack_sig_AUC',
     'attack_sig_FDIF01'
 ]
-# %% Example, plot miAUC for a single dataset, stratified by a strucutral risk metric
-dataset = 'minmax mimic2-iaccd'
-always_cols = ['model_data_param_id']
-import pylab as plt
-fig, axes = plt.subplots(len(attack_metrics), len(structural_metrics), figsize=(20, 20))
 
-for row_idx, plot_metric in enumerate(attack_metrics):
-    for col_idx, structural_metric in enumerate(structural_metrics):
-
-        temp_plot_metric = agg_df[agg_df['scenario'] == 'WorstCase'].loc[:, always_cols + [plot_metric, "dataset"]].set_index('model_data_param_id')
-        temp_structural_metric = agg_df[agg_df['scenario'] == 'Structural'].loc[:, always_cols + [structural_metric]].set_index('model_data_param_id')
-        plot_df= temp_plot_metric.join(temp_structural_metric, how='left')
-
-        
-        chart = sns.violinplot(data=plot_df, x="dataset", y=plot_metric, hue=structural_metric,
-               split=True, inner="quart", fill=False,
-               palette={1: "g", 0: ".35"}, ax=axes[row_idx, col_idx])
-        chart.set_xticklabels(chart.get_xticklabels(), rotation=45, horizontalalignment='right')
-
-        plot_title = f'{dataset}-{plot_metric}-{structural_metric}'
-        chart.set_title(plot_title)
 # %%
 # Seaborn provides really useful methods for grids of plots
-# Below attempts to use this. For a particular combination of classifier x mia_metric we
-# want a row of plots. The row should have one column per structural metric and one violin
-# plot per dataset
-# To do this, we need to pivot the relevant columns
-attack_metric = attack_metrics[0]
-cols_to_use = ['model_data_param_id', 'dataset', attack_metric] + structural_metrics
-non_pivot_df = agg_df[]
+# This code creates a dataframe that can make use of this.
+# The dataframe will have one row for every combination of:
+# - an attack metric
+# - a structural metric
+# - a dataset
+# - a model_run_param_id
+# - a scenario
+# I.e. each experiment will now be multiple rows, where each row will allow us to compare one
+# pair of attack metric and structural metric.
+
+# Columns that serve to identify an experiment. Note redundant (dataset is in the id), but
+# we need dataset for the plotting
+unique_cols = ['model_data_param_id', 'dataset']
+
+# Take only rows corresponding to structural scenarios, and just the structural columns
+structural_metric_df = agg_df[agg_df['scenario'] == 'Structural'].loc[:, unique_cols + structural_metrics]
+
+# Take only rows corresponding to the attack scenario of interest, and just the attack metric columns
+attack_metric_df = agg_df[agg_df['scenario'] == ATTACK_SCENARIO].loc[:, unique_cols + attack_metrics]
+
+# Melt both dfs to9 a long thing version where, instead of one column per metric, they have each
+# metric value for each unique experiment on a different row
+structural_metric_df = pd.melt(structural_metric_df, id_vars=unique_cols)
+attack_metric_df = pd.melt(attack_metric_df, id_vars=unique_cols)
+
+# Rename the columns to avoid a clash and make things clearer
+structural_metric_df.rename(columns={'variable': 'structural_metric_name', 'value': 'structural_metric_value'}, inplace=True)
+attack_metric_df.rename(columns={'variable': 'attack_metric_name', 'value': 'attack_metric_value'}, inplace=True)
+
+# Set the index to be the id and dataset
+structural_metric_df.set_index(unique_cols, inplace=True)
+attack_metric_df.set_index(unique_cols, inplace=True)
+
+# Do an outer join to get all combinations
+full_df = attack_metric_df.join(structural_metric_df, how='outer')
+
+# Put the index back as columns
+full_df.reset_index(inplace=True)
+# %% Make the plot
+g = sns.FacetGrid(
+    full_df,
+    col='structural_metric_name',
+    row='attack_metric_name',
+    margin_titles=True,
+    height=10,
+    sharey=False # Means that columns within a row share a y-axis, but different scales can be used on each row
+)
+
+g.map(sns.violinplot,'dataset', 'attack_metric_value', hue=full_df['structural_metric_value'], split=True,
+        inner="quart", fill=False,
+        palette={1: "g", 0: ".35"} # green when value is 1, grey otherwise
+)
