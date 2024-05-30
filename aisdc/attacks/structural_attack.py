9ec8355f8a316ef028311d3d4cb017d9ce0b0049
Jim-smith <jim-smith@users.noreply.github.com>
2024-05-30 20:00:58 +0100

    added support for sklearn MLPs

--- aisdc/attacks/structural_attack.py
+++ aisdc/attacks/structural_attack.py
@@ -20,6 +20,7 @@ from acro import ACRO
 # tree-based model types currently supported
 from sklearn.base import BaseEstimator
 from sklearn.ensemble import AdaBoostClassifier, RandomForestClassifier
+from sklearn.neural_network import MLPClassifier
 from sklearn.tree import DecisionTreeClassifier
 from xgboost.sklearn import XGBClassifier
 
@@ -184,6 +185,12 @@ def get_model_param_count(model: BaseEstimator) -> int:
         n_leaves = len(df[df.Feature == "Leaf"])
         # 2 per internal node, one per clas in leaves, one weight per tree
         n_params = 2 * (total - n_leaves) + (model.n_classes_ - 1) * n_leaves + n_trees
+
+    elif isinstance(model, MLPClassifier):
+        weights = model.coefs_  # dtype is list of numpy.ndarrays
+        biasses = model.intercepts_  # dtype is list of numpy.ndarrays
+        n_params = sum([a.size for a in weights]) + sum([a.size for a in biasses])
+
     else:
         pass
 
@@ -354,6 +361,7 @@ class StructuralAttack(Attack):
         for prob_vals in equiv_classes:
             ingroup = np.unique(np.asarray(self.yprobs == prob_vals).nonzero()[0])
             members.append(ingroup)
+        print(equiv_counts)
         return [equiv_classes, equiv_counts, members]
 
     def _get_global_metrics(self, attack_metrics: list) -> dict:
