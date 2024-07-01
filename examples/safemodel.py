77ef7f76d7fe94bfc71c9b9db7f4d7bb06a72cbf
Richard Preen <rpreen@gmail.com>
2024-07-01 20:09:54 +0100

    update safemodel example

--- examples/safemodel.py
+++ examples/safemodel.py
@@ -58,7 +58,7 @@ if __name__ == "__main__":
 
     logging.info("Wrapping the model and data in a Target object")
     target = Target(model=model)
-    target.name = "nursery"
+    target.dataset_name = "nursery"
     target.add_processed_data(X_train, y_train, X_test, y_test)
     target.add_raw_data(X, y, X_train_orig, y_train_orig, X_test_orig, y_test_orig)
     for i in range(n_features):
