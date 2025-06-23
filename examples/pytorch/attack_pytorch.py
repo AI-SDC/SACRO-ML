5616e066f39b427cf12f9f48e9b4353b87c225be
Richard Preen <rpreen@gmail.com>
2025-06-23 21:23:23 +0100

    refactor pytorch example

--- examples/pytorch/attack_pytorch.py
+++ examples/pytorch/attack_pytorch.py
@@ -2,13 +2,11 @@
 
 import logging
 
-from sacroml.attacks import (
-    attribute_attack,
-    likelihood_attack,
-    structural_attack,
-    worst_case_attack,
-)
+from sacroml.attacks.attribute_attack import AttributeAttack
+from sacroml.attacks.likelihood_attack import LIRAAttack
+from sacroml.attacks.structural_attack import StructuralAttack
 from sacroml.attacks.target import Target
+from sacroml.attacks.worst_case_attack import WorstCaseAttack
 
 output_dir = "output_pytorch"
 target_dir = "target_pytorch"
@@ -21,14 +19,14 @@ if __name__ == "__main__":
 
     logging.info("Running attacks...")
 
-    attack = worst_case_attack.WorstCaseAttack(n_reps=10, output_dir=output_dir)
+    attack = WorstCaseAttack(n_reps=10, output_dir=output_dir)
     output = attack.attack(target)
 
-    attack = structural_attack.StructuralAttack(output_dir=output_dir)
+    attack = StructuralAttack(output_dir=output_dir)
     output = attack.attack(target)
 
-    attack = attribute_attack.AttributeAttack(n_cpu=2, output_dir=output_dir)
+    attack = AttributeAttack(n_cpu=2, output_dir=output_dir)
     output = attack.attack(target)
 
-    attack = likelihood_attack.LIRAAttack(n_shadow_models=100, output_dir=output_dir)
+    attack = LIRAAttack(n_shadow_models=100, output_dir=output_dir)
     output = attack.attack(target)
