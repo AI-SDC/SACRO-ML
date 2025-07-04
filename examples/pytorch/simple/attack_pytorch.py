dc315b9a1049190e4470c44a4f905efd55a2fc3e
Richard Preen <rpreen@gmail.com>
2025-07-04 12:04:08 +0100

    update example docstrings

--- examples/pytorch/simple/attack_pytorch.py
+++ examples/pytorch/simple/attack_pytorch.py
@@ -1,4 +1,4 @@
-"""Run attacks on an example Pytorch classifier."""
+"""Example of how to run attacks on a model saved with the Target wrapper."""
 
 import logging
 
@@ -12,13 +12,14 @@ target_dir = "target_pytorch"
 
 if __name__ == "__main__":
     logging.info("Loading Target object from '%s'", target_dir)
+
     target = Target()
     target.load(target_dir)
 
     logging.info("Running attacks...")
 
     attack = WorstCaseAttack(n_reps=10, output_dir=output_dir)
-    output = attack.attack(target)
+    attack.attack(target)
 
     attack = LIRAAttack(n_shadow_models=100, output_dir=output_dir)
-    output = attack.attack(target)
+    attack.attack(target)
