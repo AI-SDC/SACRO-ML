b2d9f4956b108e26598e16f9107e0190d1add61b
shamykyzer <shamyyk@gmail.com>
2026-03-11 10:13:59 +0000

    removed imports in factory.py unrelated to openml issue

--- sacroml/attacks/factory.py
+++ sacroml/attacks/factory.py
@@ -8,7 +8,6 @@ from sacroml.attacks.attribute_attack import AttributeAttack
 from sacroml.attacks.likelihood_attack import LIRAAttack
 from sacroml.attacks.structural_attack import StructuralAttack
 from sacroml.attacks.target import Target
-from sacroml.attacks.training_data_attack import TrainingDataInModelAttack
 from sacroml.attacks.worst_case_attack import WorstCaseAttack
 from sacroml.version import __version__
 
@@ -19,7 +18,6 @@ registry: dict = {
     "attribute": AttributeAttack,
     "lira": LIRAAttack,
     "structural": StructuralAttack,
-    "trainingdata": TrainingDataInModelAttack,
     "worstcase": WorstCaseAttack,
 }
 
